07d685134a5e953404f2ceae4117cb41a65f9227
Ayavuya Genu <ayagenu@gmail.com>
2023-01-19 19:08:49 +0200

    5th commit

--- sender.js
+++ sender.js
@@ -4,37 +4,35 @@ var sqs = new AWS.SQS({
 });
 
 exports.handler = function(event, context, callback) {
-  
-  var accountId = context.invokedFunctionArn.split(":")[4];
-  var queueUrl = 'http://sqs.us-east-1.amazonaws.com/' + accountId + 'MyQueue';
-  // response and status of HTTP endpoint
-  var responseBody = {
-    message: ''
-  };
+    var accountId = context.invokedFunctionArn.split(":")[4];
+    var queueUrl = 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/MyQueue';
+    // response and status of HTTP endpoint
+    var responseBody = {
+        message: ''
+    };
+    var responseCode = 200;
+    // SQS message parameters
+    var params = {
+        MessageBody: event.body,
+        QueueUrl: queueUrl
+    };
+    sqs.sendMessage(params, function(err, data) {
+        if (err) {
+            console.log('error:', "failed to send message" + err);
+            var responseCode = 500;
+        } else {
+            console.log('data:', data.MessageId);
+            responseBody.message = 'Sent to ' + queueUrl;
+            responseBody.messageId = data.MessageId;
+        }
+        var response = {
+            statusCode: responseCode,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(responseBody)
+        };
 
-var responseCode = 200;
-//SQS message parameters
-var params = {
-  MessageBody: event.body,
-  QueueUrl: queueUrl
-};
-
-sqs.sendMessage(params, function(err, data){
-  if (err) {
-    console.log('error:', "failed to send message" + err);
-    var responseCode = 500;
-  } else{
-      console.log('data:', data.MessageId);
-      responseBody.message = 'Sent to ' + queueUrl;
-      responseBody.MessageId = data.MessageId;
-  }
-  var response = {
-    statusCode: responseCode,
-    headers:{
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(responseBody)
-  };
-  callback(null, response);
-});
+        callback(null, response);
+    });
 }
